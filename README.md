6c4fb69cf47302af2c01db5ade8c40fc7d77cde0
Xin Deng <xind1470@gmail.com>
2023-10-30 13:31:21 -0700

    set up each file with a template

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cookie-stand
-Code fellows 201 lab 6, 7, 8, 9, 10 Salmon Cookies
+# Lab 6, 7, 8, 9, 10 Salmon Cookies
+
+## Salmon Cookies Pt.1 (HTML/CSS/JS)
+
+### Author: Xin Deng
+
+### Links and Resources
+
+1. [Setting Up a New Code 201 Project](https://codefellows.github.io/code-201-guide/curriculum/class-02/project-setup)
+
+
+### Lighthouse Accessibility Report Score
+
+![Lighthouse report for Lab 2](img/lighthouse2.png)
+
+### Reflections and Comments
+
+1. How did this go, overall?
+  
+
+2. What observations or questions do you have about what you’ve learned so far?
+
+
+3. How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+ 
---
+++ js/app.js
@@ -0,0 +1,49 @@
+// create separate JS object literals for each shop location. Each location will be responsible for generating sales data and providing output on an html document. You should be able to perform the following tasks in your javascript file:
+
+// Store the min/max hourly customers, and the average cookies per customer, in object properties.
+// Generate a random number of customers that can be used to simulate hourly sales, using Objects/Math/random
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated.
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location.
+// Display the values of each array as unordered lists in the browser.
+// Calculating the sum of these hourly totals; your output for each location should look like this:
+
+
+// Make sure to make each location is its own JavaScript object.
+
+// Location	Min / Cust	Max / Cust	Avg Cookie / Sale
+// Seattle	23	65	6.3
+// Tokyo	3	24	1.2
+// Dubai	11	38	3.7
+// Paris	20	38	2.3
+// Lima	2	16	4.6
+
+'use strict';
+
+
+// seattle object 
+const seattle = {
+
+}
+
+
+// seattle object 
+const Tokyo = {
+  
+}
+
+
+// seattle object 
+const Dubai = {
+  
+}
+
+// seattle object 
+const Paris = {
+  
+}
+
+
+// seattle object 
+const Lima = {
+  
+}
