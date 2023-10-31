6131d01755c417ce5e7ccdebdafaedc6981d831b
Xin Deng <xind1470@gmail.com>
2023-10-31 16:04:11 -0700

    Finished figuring out table elements for one row

--- js/app.js
+++ js/app.js
@@ -17,48 +17,69 @@ let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 let storeLocation = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
 
 
-
 //Constructor function for creating location objects
 function Location (name, min, max, avg, openHours, phone, address){
-  this.name = name,
-  this.min = min,
-  this.max = max,
-  this.avg = avg,
-  this.openHours = openHours,
-  this.phone = phone,
-  this.address = address,
-  this.cookiesPerHour = [],
-  this.totalCookies = 0,
-  this.customersPerHour = []
-}
-
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.openHours = openHours;
+  this.phone = phone;
+  this.address = address;
+  this.cookiesPerHour = [];
+  this.totalCookies = 0;
+  this.customersPerHour = [];
+};
+//Adding calRandomCustomers function into Location constructor
   Location.prototype.calRandomCustomers = function (){
     return Math.floor(Math.random()*(this.max - this.min +1) + this.min);
   };
+  //Adding calCookiesPerHour function into Location constructor
   Location.prototype.calCookiesPerHour = function(){
     for ( let i=0; i<hours.length;i++){
-      const randomCustomers = this.calRandomCustomers; 
+      const randomCustomers = this.calRandomCustomers(); 
       const cookiesSold = Math.ceil(randomCustomers * this.avg);
     (this.cookiesPerHour.push(cookiesSold)); //(push adds to end of array)
      (this.totalCookies += cookiesSold);
   }
+};
+//Adding render function in to Location constructor
+const tableElement = document.getElementById('Table');
+Location.prototype.render = function(){
+  this.calCookiesPerHour();
+  let locationRows = document.createElement('tr');
+  tableElement.appendChild(locationRows);
+  let locationName = document.createElement('td');
+  locationName.textContent = this.name;
+  locationRows.appendChild(locationName);
+  for (let i=0; i<hours.length;i++){
+    let hourlyData = document.createElement('td');
+    console.log(hourlyData.textContent = this.cookiesPerHour[i]);
+    locationRows.appendChild(hourlyData);
+  }
+
+
+
+
+
 }
 
 
-
-
-
+// locations time across daily location total header
+// cites 
+// total header
 
 
 
 
 //Creating new objects using the constructor 
-let Seattle = new Store('Seattle', 23, 65, 6.3, '6am - 7pm', '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121');
-let Tokyo = new Store('Tokyo', 3, 24, 1.2, '6am - 7pm', '222-222-2222', '1 Chrome-1-2 Oshiage, Sumida CountQueuingStrategy, Tokyo 131-8634');
-let Dubai = new Store('Dubai', 11, 38, 3.7, '6am - 7pm', '333-333-3333', '1 Sheikh Mohammed bin Rashid Blvd - Dubai');
-let Paris = new Store('Paris', 20, 38, 2.3, '6am - 7pm', '444-444-4444', 'Champ de MediaStream, 5 Avenue Anatole France, 75007');
-let Lima = new Store('Lima', 2, 16, 4.6, '6am - 7pm', '555-555-5555', 'Cache. Gral. Borgoño cuadra 8, Miraflores 15074');
+let Seattle = new Location('Seattle', 23, 65, 6.3, '6am - 7pm', '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121');
+// let Tokyo = new Location('Tokyo', 3, 24, 1.2, '6am - 7pm', '222-222-2222', '1 Chrome-1-2 Oshiage, Sumida CountQueuingStrategy, Tokyo 131-8634');
+// let Dubai = new Location('Dubai', 11, 38, 3.7, '6am - 7pm', '333-333-3333', '1 Sheikh Mohammed bin Rashid Blvd - Dubai');
+// let Paris = new Location('Paris', 20, 38, 2.3, '6am - 7pm', '444-444-4444', 'Champ de MediaStream, 5 Avenue Anatole France, 75007');
+// let Lima = new Location('Lima', 2, 16, 4.6, '6am - 7pm', '555-555-5555', 'Cache. Gral. Borgoño cuadra 8, Miraflores 15074');
 
+Seattle.render();
 
 
 /*
