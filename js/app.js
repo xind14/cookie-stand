9ac4fb02e2f038abb23c09946043ea1d2071c337
Xin Deng <xind1470@gmail.com>
2023-10-31 13:23:30 -0700

    created new branch and set up files for lab 7

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 
 'use strict';
 
+
+
+/*
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
 
 const randomCustomers= function () {
@@ -178,4 +181,4 @@ const limaLocation = {
     };
     limaLocation.randomCookiesNumber(); 
    limaLocation.render(); 
-
+*/
