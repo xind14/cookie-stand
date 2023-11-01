61a81976246b5f9c8f227e55a74883e4117f0b36
Xin Deng <xind1470@gmail.com>
2023-10-31 19:32:57 -0700

    added footer header

--- js/app.js
+++ js/app.js
@@ -14,8 +14,14 @@ NOTE: Please use a header cell for both the header row ( containing store hours 
 
 let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
-let storeLocation = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
+//Creating new objects using the constructor 
+let Seattle = new Location('Seattle', 23, 65, 6.3, '6am - 7pm', '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121');
+let Tokyo = new Location('Tokyo', 3, 24, 1.2, '6am - 7pm', '222-222-2222', '1 Chrome-1-2 Oshiage, Sumida CountQueuingStrategy, Tokyo 131-8634');
+let Dubai = new Location('Dubai', 11, 38, 3.7, '6am - 7pm', '333-333-3333', '1 Sheikh Mohammed bin Rashid Blvd - Dubai');
+let Paris = new Location('Paris', 20, 38, 2.3, '6am - 7pm', '444-444-4444', 'Champ de MediaStream, 5 Avenue Anatole France, 75007');
+let Lima = new Location('Lima', 2, 16, 4.6, '6am - 7pm', '555-555-5555', 'Cache. Gral. Borgoño cuadra 8, Miraflores 15074');
 
+let storeLocation = [Seattle, Tokyo, Dubai, Paris, Lima]
 
 //Constructor function for creating location objects
 function Location (name, min, max, avg, openHours, phone, address){
@@ -54,7 +60,7 @@ Location.prototype.render = function(){
   locationRows.appendChild(locationName);
   for (let i=0; i<hours.length;i++){
     let hourlyData = document.createElement('td');
-    console.log(hourlyData.textContent = this.cookiesPerHour[i]);
+   hourlyData.textContent = this.cookiesPerHour[i];
     locationRows.appendChild(hourlyData);
   }
    let locationTotal = document.createElement('th');
@@ -63,7 +69,6 @@ locationRows.appendChild(locationTotal);
 };
 
 //Header function
-
 let renderHeader = function (){
   let headerRow = document.createElement("tr");
   tableElement.appendChild(headerRow);
@@ -77,24 +82,40 @@ locationHeader.textContent = 'Locations';
     hoursHeader = document.createElement('th');
     hoursHeader.textContent = hours[i];
    headerRow.appendChild(hoursHeader);
-} 
+  } 
+  let dailyTotalHeader = document.createElement('th');
+  dailyTotalHeader.textContent = 'Daily Location Total';
+  headerRow.appendChild(dailyTotalHeader);
+};
+
+//Footer function
+let renderFooter = function (){
+  let footerRow = document.createElement("tr");
+  tableElement.appendChild(footerRow);
+
+  let locationFooter = document.createElement('th');
+locationFooter.textContent = 'Hourly Totals for All Locations';
+  footerRow.appendChild(locationFooter);
+
+  let dailyTotal = 0;
+  for (let i = 0; i < hours.length; i++) {
+    let hourlyTotal = 0;
+    for (let j = 0; j < storeLocation.length; j++) {
+      hourlyTotal += storeLocation[j].cookiesPerHour[i];
+      dailyTotal += storeLocation[j].cookiesPerHour[i];
+    }  
+    let hoursFooter = document.createElement('th');
+    hoursFooter.textContent = hourlyTotal;
+   footerRow.appendChild(hoursFooter);
+  }
+   let totalTotal = document.createElement('th');
+   totalTotal.textContent = dailyTotal;
+  footerRow.appendChild(totalTotal);
 }
 
 
+//Render All
 renderHeader();
-
-
-// locations time across daily location total header
-// cites 
-// total header
-
-
-//Creating new objects using the constructor 
-let Seattle = new Location('Seattle', 23, 65, 6.3, '6am - 7pm', '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121');
-let Tokyo = new Location('Tokyo', 3, 24, 1.2, '6am - 7pm', '222-222-2222', '1 Chrome-1-2 Oshiage, Sumida CountQueuingStrategy, Tokyo 131-8634');
-let Dubai = new Location('Dubai', 11, 38, 3.7, '6am - 7pm', '333-333-3333', '1 Sheikh Mohammed bin Rashid Blvd - Dubai');
-let Paris = new Location('Paris', 20, 38, 2.3, '6am - 7pm', '444-444-4444', 'Champ de MediaStream, 5 Avenue Anatole France, 75007');
-let Lima = new Location('Lima', 2, 16, 4.6, '6am - 7pm', '555-555-5555', 'Cache. Gral. Borgoño cuadra 8, Miraflores 15074');
 
 Seattle.render();
 Tokyo.render();
@@ -102,6 +123,7 @@ Dubai.render();
 Paris.render();
 Lima.render();
 
+renderFooter();
 
 /*
 
